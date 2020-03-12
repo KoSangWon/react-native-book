62dc99df677b3e596075e6d9b7ceae32ae76efda
KoSangWon <jackoss@naver.com>
2020-03-12 22:08:15 +0900

    add BookList.js

--- first-project/App.js
+++ first-project/App.js
@@ -3,11 +3,12 @@ import { StyleSheet, Text, View } from 'react-native';
 import WeatherProject from "./WeatherProject";
 import Button from "./PressDemo"
 import Simple from "./src/bestsellers/SimpleList"
+import BookItem from "./src/bestsellers/BookItem"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Simple></Simple>
+      <BookItem></BookItem>
     </View>
   );
 }
